8194b5ddb6d3249ea7d58c999852c9d36cccb296
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2023-04-20 16:39:48 -0230

    altered splash screren and updated router

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -7,10 +7,17 @@ const Splash = () => {
     return (
         <>
             <div id="mainlogo">
-                
-            <img className="logosimple" src={SimpleLogo} />
-            <img className="logomain" src={Logo} />
-                
+                <img className="logosimple" src={SimpleLogo} />
+                <img className="logomain" src={Logo} />
+            </div>
+            <div id="introcredentials" className="introbegin animatecredentials">
+                Malinda McDonald - Master Colorist and Stylist
+            </div>
+            <div id="introhook1" className="animatehook1">
+                Bring out your color and style
+            </div>
+            <div id="introhook2" className="animatehook2">
+                Because when you look good, you feel good
             </div>
         </>
     )
