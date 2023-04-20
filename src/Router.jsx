4a9df670bf91bb77a1f8b9978c1aaee9d9f4741f
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2023-04-20 18:12:43 -0230

    Updated media queries for splash

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,10 +10,6 @@ export const router = createBrowserRouter([
     element: <Splash />,
   },
   {
-    path: "/Splash",
-    element: <Splash />,
-  },
-  {
       path: "/SvgLogo",
       element: <SvgLogo />,
   },
--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,15 +1,21 @@
 import "./Splash.css";
 import Logo from "../../images/Logo.svg";
 import SimpleLogo from "../../images/SimpleLogo.svg";
+import { useNavigate } from "react-router-dom";
 
 
 const Splash = () => {
+    const navigate = useNavigate();
+
     return (
         <>
+        <a href="" onClick={()=>navigate("/Home")}>
+        <div id="introcontainer" onClick={()=>navigate("/")}>
             <div id="mainlogo">
                 <img className="logosimple" src={SimpleLogo} />
                 <img className="logomain" src={Logo} />
             </div>
+            {/*
             <div id="introcredentials" className="introbegin animatecredentials">
                 Malinda McDonald - Master Colorist and Stylist
             </div>
@@ -19,6 +25,9 @@ const Splash = () => {
             <div id="introhook2" className="animatehook2">
                 Because when you look good, you feel good
             </div>
+        */}
+            </div>
+            </a>
         </>
     )
 }
