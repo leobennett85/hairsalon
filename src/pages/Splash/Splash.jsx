3889c145e8904f17d0fc31c9665890d7ca0f8787
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2023-04-22 17:49:57 -0230

    Tested new width for screen sizes

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -9,25 +9,13 @@ const Splash = () => {
 
     return (
         <>
-        <a href="" onClick={()=>navigate("/Home")}>
+        {/*<a href="" onClick={()=>navigate("/Home")}>*/}
         <div id="introcontainer" onClick={()=>navigate("/")}>
-            <div id="mainlogo">
-                <img className="logosimple" src={SimpleLogo} />
-                <img className="logomain" src={Logo} />
-            </div>
-            {/*
-            <div id="introcredentials" className="introbegin animatecredentials">
-                Malinda McDonald - Master Colorist and Stylist
-            </div>
-            <div id="introhook1" className="animatehook1">
-                Bring out your color and style
-            </div>
-            <div id="introhook2" className="animatehook2">
-                Because when you look good, you feel good
-            </div>
-        */}
-            </div>
-            </a>
+            <img className="logosimple" src={SimpleLogo} />
+            <img className="logomain" src={Logo} />
+            <span className="introname">Malinda McDonald</span>
+        </div>
+        {/*</a>*/}
         </>
     )
 }
