7e3dc5075bc08e9d0cecb480c508bdf7c4265e4c
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2023-05-05 19:07:08 -0230

    Fixing nameswipe

--- src/components/Nameswipe/Nameswipe.jsx
+++ src/components/Nameswipe/Nameswipe.jsx
@@ -1,10 +1,25 @@
 import "./Nameswipe.css";
+import NameSwipe from "../../images/name.svg";
+import { motion } from "framer-motion";
+
 
 const Nameswipe = () => {
 return (
     <>
-    <div className="firstname">Malinda</div>
-    <div className="lastName">Mcdonald</div>
+    <div id="nameSwipeContainer">
+        <div id="nameSwipe">
+        <motion.img
+            className="nameSwipe"
+            initial={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 6,
+                delay: 0.5,
+                ease: [0.25, 0.50, 0.75, 1]
+            }}
+            src={NameSwipe} />
+        </div>
+    </div>
     </>
 )
 }
