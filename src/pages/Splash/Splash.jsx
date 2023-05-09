c7ccd10a45fbd00949f0b30b91282a699384d787
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2023-05-09 16:17:27 -0230

    Updated new navigation on Splash.jsx

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,12 +1,13 @@
 import "./Splash.css";
 import Nameswipe from "../../components/Nameswipe/Nameswipe";
 import SplashLogo from "../../components/SplashLogo/SplashLogo";
-
+import { useNavigate } from "react-router-dom";
 
 const Splash = () => {
-
+    const navigate = useNavigate();
     return (
         <>
+        <a href="" onClick={()=>navigate("/Home")} className="text-navbar">Home</a>
         <div id="introContainer">
             <div id="splashLogoContainer">
                 <SplashLogo />
